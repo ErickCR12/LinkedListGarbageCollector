2a023682956f775176bbf0eca81a4baee7fdcc99
“erickCR12” <erick.cr12@outlook.com>
2019-03-09 17:58:47 -0600

    Added full project

---
+++ cmake-build-debug/CMakeFiles/LinkedListGarbageCollector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkedListGarbageCollector.dir/main.cpp.o"
+  "LinkedListGarbageCollector.pdb"
+  "LinkedListGarbageCollector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinkedListGarbageCollector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/LinkedListGarbageCollector.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LinkedListGarbageCollector.
+# This may be replaced when dependencies are built.
